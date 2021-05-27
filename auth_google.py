4393bd4b19571ba818e596b37ed9530afdd21a6d
Kerem <63135398+Kerem-git@users.noreply.github.com>
2021-05-27 21:36:03 +0300

    Update auth_google.py

--- auth_google.py
+++ auth_google.py
@@ -3,7 +3,13 @@ from time import sleep
 import requests
 from bs4 import BeautifulSoup
 import sys
+import smtplib
+
 print("bot has been started.")
+email = "mahmutcarlsen@gmail.com"
+password = "Kerem2894" 
+mails= ["keremtowork@gmail.com","keremtowork@gmail.com"]
+
 for i in range(999999999999999):
     url= "https://www.timeanddate.com/worldclock/turkey/adana"
     date = requests.get(url)
@@ -12,10 +18,15 @@ for i in range(999999999999999):
     print(day)
     print("I'm still working")
     key = "BQ5A K56L 4IFT ACEZ QODW EF5X TB2N MBOU"
-    sleep(1)
+    sleep(30)
     if "Thursday" in day:
-        print(key)
-        sys.stdout.write("\033[F")
-        sleep(3)
-        sys.stdout.write("\033[K")
+        with smtplib.SMTP("smtp.gmail.com",587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+            smtp.login(email, password)
+            subject = "Auth Kodun."
+            body = key
+            msg = f"Subject: {subject}\n\n{body}"
+            smtp.sendmail(email,mails[0],msg)
 
