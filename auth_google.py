dbd6089a73d45e34c0eaf31a46adef88cc904fe0
Kerem <63135398+Kerem-git@users.noreply.github.com>
2021-05-27 03:15:47 +0300

    Add files via upload

---
+++ auth_google.py
@@ -0,0 +1,30 @@
+from bs4.element import Declaration
+from cryptography.fernet import Fernet
+from time import sleep
+import requests
+from bs4 import BeautifulSoup
+import sys
+print("bot has been started.")
+for i in range(999999999999999):
+    url= "https://www.timeanddate.com/worldclock/turkey/adana"
+    date = requests.get(url)
+    soup = BeautifulSoup(date.content,"html5lib")
+    day = soup.find("span",{"id":"ctdat"}).text
+    print(day)
+    
+    message = "BQ5A  K56L  4IFT  ACEZ  QODW  EF5X  TB2N  MBOU"
+    
+    key = Fernet.generate_key()
+    
+    fernet = Fernet(key)
+
+    encMessage = fernet.encrypt(message.encode())
+
+    print("encrypted string: ", encMessage)
+    sleep(1)
+    decMessage = fernet.decrypt(encMessage).decode()
+    if "Perşembe" in day:
+        print("decrypted string: ", decMessage)
+        sys.stdout.write("\033[F")
+        sleep(43000)
+        sys.stdout.write("\033[K")
